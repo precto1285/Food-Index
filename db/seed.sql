214b62a4b6e20ae6e6d6966d7db6a4fbdfee4b42
Philippe Recto <precto1285@gmal.com>
2018-05-16 09:48:46 -0700

    Added Seeds info

---
+++ db/seed.sql
@@ -0,0 +1,53 @@
+INSERT INTO foodIndex ('Food', 'Serving Size', 'Calories', 'Fat(grams)', 'Carbohydrates(grams)', 'Sugar(grams)', 'Protein', 'Sodium(milligrams', 'Fiber(grams)')
+VALUES ('Orowheat Double-Fiber bread', '2 slices (76g)', 160, 3, 34, 4, 8, 260, 10);
+
+INSERT INTO foodIndex ('Food', 'Serving Size', 'Calories', 'Fat(grams)', 'Carbohydrates(grams)', 'Sugar(grams)', 'Protein', 'Sodium(milligrams', 'Fiber(grams)')
+VALUES ('Gatorade Frost', '12 fl oz', 80, 0, 21, 21, 0, 0, 0);
+
+INSERT INTO foodIndex ('Food', 'Serving Size', 'Calories', 'Fat(grams)', 'Carbohydrates(grams)', 'Sugar(grams)', 'Protein', 'Sodium(milligrams', 'Fiber(grams)')
+VALUES ('Glaceau Vitamin Water','20 fl oz', 120, 0, 33, 32, 0, 0, 0);
+
+INSERT INTO foodIndex ('Food', 'Serving Size', 'Calories', 'Fat(grams)', 'Carbohydrates(grams)', 'Sugar(grams)', 'Protein', 'Sodium(milligrams', 'Fiber(grams)')
+VALUES ('Special-K Berries Cereal', '1 cup (31g)', 110, 0, 27, 9, 2, 190, 3);
+
+INSERT INTO foodIndex ('Food', 'Serving Size', 'Calories', 'Fat(grams)', 'Carbohydrates(grams)', 'Sugar(grams)', 'Protein', 'Sodium(milligrams', 'Fiber(grams)')
+VALUES ('Lactaid Milk', '1 cup (240ml)', 130, 5, 13, 12, 8, 125, 0);
+
+INSERT INTO foodIndex ('Food', 'Serving Size', 'Calories', 'Fat(grams)', 'Carbohydrates(grams)', 'Sugar(grams)', 'Protein', 'Sodium(milligrams', 'Fiber(grams)')
+VALUES ('Provolone Cheese', '1 slice (19g)', 70, 5, 0, 0, 5, 190, 3);
+
+INSERT INTO foodIndex ('Food', 'Serving Size', 'Calories', 'Fat(grams)', 'Carbohydrates(grams)', 'Sugar(grams)', 'Protein', 'Sodium(milligrams', 'Fiber(grams)')
+VALUES ('Chocolate Popsicle', '2 pops (79g)', 80, 2, 18, 0, 0, 46, 50, 0);
+
+INSERT INTO foodIndex ('Food', 'Serving Size', 'Calories', 'Fat(grams)', 'Carbohydrates(grams)', 'Sugar(grams)', 'Protein', 'Sodium(milligrams', 'Fiber(grams)')
+VALUES ('Jifs Natural Low-Sodium Peanut Butter', '2 Tbsp', 190, 16, 8, 5, 7, 65, 2);
+
+INSERT INTO foodIndex ('Food', 'Serving Size', 'Calories', 'Fat(grams)', 'Carbohydrates(grams)', 'Sugar(grams)', 'Protein', 'Sodium(milligrams', 'Fiber(grams)')
+VALUES ('Sugar-Free Strawberry Jam', '1 Tbsp (17g)', 10, 0, 5, 0, 0, 0, 3);
+
+INSERT INTO foodIndex ('Food', 'Serving Size', 'Calories', 'Fat(grams)', 'Carbohydrates(grams)', 'Sugar(grams)', 'Protein', 'Sodium(milligrams', 'Fiber(grams)')
+VALUES ('Milk Fish(Bangus)', '1 fish (200g)',340, 18, 0, 0, 46, 50, 0);
+
+INSERT INTO foodIndex ('Food', 'Serving Size', 'Calories', 'Fat(grams)', 'Carbohydrates(grams)', 'Sugar(grams)', 'Protein', 'Sodium(milligrams', 'Fiber(grams)')
+VALUES ('Grandmas Oatmeal Cookies', '1 pkg (2 cookies)', 310, 12, 46, 24, 4, 390, 3);
+
+INSERT INTO foodIndex ('Food', 'Serving Size', 'Calories', 'Fat(grams)', 'Carbohydrates(grams)', 'Sugar(grams)', 'Protein', 'Sodium(milligrams', 'Fiber(grams)')
+VALUES ('Trail Mix', '1pkg (57g)', 270, 18, 27, 20, 8, 110, 3);
+
+INSERT INTO foodIndex ('Food', 'Serving Size', 'Calories', 'Fat(grams)', 'Carbohydrates(grams)', 'Sugar(grams)', 'Protein', 'Sodium(milligrams', 'Fiber(grams)')
+VALUES ('Banana', '1 banana', 105, 0.4, 27, 14, 1.3, 1, 3.1);
+
+INSERT INTO foodIndex ('Food', 'Serving Size', 'Calories', 'Fat(grams)', 'Carbohydrates(grams)', 'Sugar(grams)', 'Protein', 'Sodium(milligrams', 'Fiber(grams)')
+VALUES ('Coffee', '1 cup', 15, 0, 3, 0, 1, 0, 0);
+
+INSERT INTO foodIndex ('Food', 'Serving Size', 'Calories', 'Fat(grams)', 'Carbohydrates(grams)', 'Sugar(grams)', 'Protein', 'Sodium(milligrams', 'Fiber(grams)')
+VALUES ('Splenda', '1 packet 1g)', 0, 0, 0.5, 0, 0, 0, 0);
+
+INSERT INTO foodIndex ('Food', 'Serving Size', 'Calories', 'Fat(grams)', 'Carbohydrates(grams)', 'Sugar(grams)', 'Protein', 'Sodium(milligrams', 'Fiber(grams)')
+VALUES ('Non-Dairy Creamer', 'Tsp (13.5g)', 10, 0, 1, 0, 0, 0, 0, 0);
+
+INSERT INTO foodIndex ('Food', 'Serving Size', 'Calories', 'Fat(grams)', 'Carbohydrates(grams)', 'Sugar(grams)', 'Protein', 'Sodium(milligrams', 'Fiber(grams)')
+VALUES ('Olive Oil', '1 Tbsp (13.5g)', 119, 14, 0, 0, 0, 0, 0);
+
+INSERT INTO foodIndex ('Food', 'Serving Size', 'Calories', 'Fat(grams)', 'Carbohydrates(grams)', 'Sugar(grams)', 'Protein', 'Sodium(milligrams', 'Fiber(grams)')
+VALUES ('Cuties', '2 small', 80, 1, 17, 13, 1, 0, 4);
--- server.js
+++ server.js
@@ -1,10 +1,37 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
+
 var app = express();
 var PORT = process.env.PORT || 8080;
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.unsubscribe(bodyParser.json());
+
+var exphbs = require('express-handlebars');
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "foodIndex_db"
+});
+
+connection.connect(function(err) {
+    if (err) {
+        console.log("error connecting: " + err.stack);
+        return;
+    }
+
+    console.log("connected as id " + connection.threadId);
+});
+
 app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);    
+    console.log("Server listening on: http://localhost: " + PORT);    
 });
 

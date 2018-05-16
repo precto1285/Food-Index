54d8f3c972eecc70c1f89b6e63eaea4237c3f73d
Philippe Recto <precto1285@gmal.com>
2018-05-16 11:09:40 -0700

    changes to sql files

--- db/schema.sql
+++ db/schema.sql
@@ -4,13 +4,13 @@ USE foodIndex_db;
 
 CREATE TABLE foodIndex 
 (
-    'Food' VARCHAR(50) NOT NULL,
-    'Serving Size' VARCHAR(50) NOT NULL,
-    'Calories' INT NOT NULL,
-    'Fat(grams)' INT NOT NULL,
-    'Carbohydrates(grams0' INT NOT NULL,
-    'Sugar(grams)' INT NOT NULL,
-    'Protein' INT NOT NULL,
-    'Sodium(milligrams)' INT NOT NULL,
-    'Fiber(grams)' INT NOT NULL
+    Food VARCHAR(50) NOT NULL,
+    Serving_Size VARCHAR(50) NOT NULL,
+    Calories INT NOT NULL,
+    Fat_grams INT NOT NULL,
+    Carbohydrates_grams INT NOT NULL,
+    Sugar_grams INT NOT NULL,
+    Protein INT NOT NULL,
+    Sodium_milligrams INT NOT NULL,
+    Fiber_grams INT NOT NULL
 );
--- db/seed.sql
+++ db/seed.sql
@@ -1,53 +1,53 @@
-INSERT INTO foodIndex ('Food', 'Serving Size', 'Calories', 'Fat(grams)', 'Carbohydrates(grams)', 'Sugar(grams)', 'Protein', 'Sodium(milligrams', 'Fiber(grams)')
+INSERT INTO foodIndex (Food, Serving_Size, Calories, Fat_grams, Carbohydrates_grams, Sugar_grams, Protein, Sodium_milligrams, Fiber_grams)
 VALUES ('Orowheat Double-Fiber bread', '2 slices (76g)', 160, 3, 34, 4, 8, 260, 10);
 
-INSERT INTO foodIndex ('Food', 'Serving Size', 'Calories', 'Fat(grams)', 'Carbohydrates(grams)', 'Sugar(grams)', 'Protein', 'Sodium(milligrams', 'Fiber(grams)')
+INSERT INTO foodIndex (Food, Serving_Size, Calories, Fat_grams, Carbohydrates_grams, Sugar_grams, Protein, Sodium_milligrams, Fiber_grams)
 VALUES ('Gatorade Frost', '12 fl oz', 80, 0, 21, 21, 0, 0, 0);
 
-INSERT INTO foodIndex ('Food', 'Serving Size', 'Calories', 'Fat(grams)', 'Carbohydrates(grams)', 'Sugar(grams)', 'Protein', 'Sodium(milligrams', 'Fiber(grams)')
+INSERT INTO foodIndex (Food, Serving_Size, Calories, Fat_grams, Carbohydrates_grams, Sugar_grams, Protein, Sodium_milligrams, Fiber_grams)
 VALUES ('Glaceau Vitamin Water','20 fl oz', 120, 0, 33, 32, 0, 0, 0);
 
-INSERT INTO foodIndex ('Food', 'Serving Size', 'Calories', 'Fat(grams)', 'Carbohydrates(grams)', 'Sugar(grams)', 'Protein', 'Sodium(milligrams', 'Fiber(grams)')
+INSERT INTO foodIndex (Food, Serving_Size, Calories, Fat_grams, Carbohydrates_grams, Sugar_grams, Protein, Sodium_milligrams, Fiber_grams)
 VALUES ('Special-K Berries Cereal', '1 cup (31g)', 110, 0, 27, 9, 2, 190, 3);
 
-INSERT INTO foodIndex ('Food', 'Serving Size', 'Calories', 'Fat(grams)', 'Carbohydrates(grams)', 'Sugar(grams)', 'Protein', 'Sodium(milligrams', 'Fiber(grams)')
+INSERT INTO foodIndex (Food, Serving_Size, Calories, Fat_grams, Carbohydrates_grams, Sugar_grams, Protein, Sodium_milligrams, Fiber_grams)
 VALUES ('Lactaid Milk', '1 cup (240ml)', 130, 5, 13, 12, 8, 125, 0);
 
-INSERT INTO foodIndex ('Food', 'Serving Size', 'Calories', 'Fat(grams)', 'Carbohydrates(grams)', 'Sugar(grams)', 'Protein', 'Sodium(milligrams', 'Fiber(grams)')
+INSERT INTO foodIndex (Food, Serving_Size, Calories, Fat_grams, Carbohydrates_grams, Sugar_grams, Protein, Sodium_milligrams, Fiber_grams)
 VALUES ('Provolone Cheese', '1 slice (19g)', 70, 5, 0, 0, 5, 190, 3);
 
-INSERT INTO foodIndex ('Food', 'Serving Size', 'Calories', 'Fat(grams)', 'Carbohydrates(grams)', 'Sugar(grams)', 'Protein', 'Sodium(milligrams', 'Fiber(grams)')
+INSERT INTO foodIndex (Food, Serving_Size, Calories, Fat_grams, Carbohydrates_grams, Sugar_grams, Protein, Sodium_milligrams, Fiber_grams)
 VALUES ('Chocolate Popsicle', '2 pops (79g)', 80, 2, 18, 0, 0, 46, 50, 0);
 
-INSERT INTO foodIndex ('Food', 'Serving Size', 'Calories', 'Fat(grams)', 'Carbohydrates(grams)', 'Sugar(grams)', 'Protein', 'Sodium(milligrams', 'Fiber(grams)')
+INSERT INTO foodIndex (Food, Serving_Size, Calories, Fat_grams, Carbohydrates_grams, Sugar_grams, Protein, Sodium_milligrams, Fiber_grams)
 VALUES ('Jifs Natural Low-Sodium Peanut Butter', '2 Tbsp', 190, 16, 8, 5, 7, 65, 2);
 
-INSERT INTO foodIndex ('Food', 'Serving Size', 'Calories', 'Fat(grams)', 'Carbohydrates(grams)', 'Sugar(grams)', 'Protein', 'Sodium(milligrams', 'Fiber(grams)')
+INSERT INTO foodIndex (Food, Serving_Size, Calories, Fat_grams, Carbohydrates_grams, Sugar_grams, Protein, Sodium_milligrams, Fiber_grams)
 VALUES ('Sugar-Free Strawberry Jam', '1 Tbsp (17g)', 10, 0, 5, 0, 0, 0, 3);
 
-INSERT INTO foodIndex ('Food', 'Serving Size', 'Calories', 'Fat(grams)', 'Carbohydrates(grams)', 'Sugar(grams)', 'Protein', 'Sodium(milligrams', 'Fiber(grams)')
+INSERT INTO foodIndex (Food, Serving_Size, Calories, Fat_grams, Carbohydrates_grams, Sugar_grams, Protein, Sodium_milligrams, Fiber_grams)
 VALUES ('Milk Fish(Bangus)', '1 fish (200g)',340, 18, 0, 0, 46, 50, 0);
 
-INSERT INTO foodIndex ('Food', 'Serving Size', 'Calories', 'Fat(grams)', 'Carbohydrates(grams)', 'Sugar(grams)', 'Protein', 'Sodium(milligrams', 'Fiber(grams)')
+INSERT INTO foodIndex (Food, Serving_Size, Calories, Fat_grams, Carbohydrates_grams, Sugar_grams, Protein, Sodium_milligrams, Fiber_grams)
 VALUES ('Grandmas Oatmeal Cookies', '1 pkg (2 cookies)', 310, 12, 46, 24, 4, 390, 3);
 
-INSERT INTO foodIndex ('Food', 'Serving Size', 'Calories', 'Fat(grams)', 'Carbohydrates(grams)', 'Sugar(grams)', 'Protein', 'Sodium(milligrams', 'Fiber(grams)')
+INSERT INTO foodIndex (Food, Serving_Size, Calories, Fat_grams, Carbohydrates_grams, Sugar_grams, Protein, Sodium_milligrams, Fiber_grams)
 VALUES ('Trail Mix', '1pkg (57g)', 270, 18, 27, 20, 8, 110, 3);
 
-INSERT INTO foodIndex ('Food', 'Serving Size', 'Calories', 'Fat(grams)', 'Carbohydrates(grams)', 'Sugar(grams)', 'Protein', 'Sodium(milligrams', 'Fiber(grams)')
+INSERT INTO foodIndex (Food, Serving_Size, Calories, Fat_grams, Carbohydrates_grams, Sugar_grams, Protein, Sodium_milligrams, Fiber_grams)
 VALUES ('Banana', '1 banana', 105, 0.4, 27, 14, 1.3, 1, 3.1);
 
-INSERT INTO foodIndex ('Food', 'Serving Size', 'Calories', 'Fat(grams)', 'Carbohydrates(grams)', 'Sugar(grams)', 'Protein', 'Sodium(milligrams', 'Fiber(grams)')
+INSERT INTO foodIndex (Food, Serving_Size, Calories, Fat_grams, Carbohydrates_grams, Sugar_grams, Protein, Sodium_milligrams, Fiber_grams)
 VALUES ('Coffee', '1 cup', 15, 0, 3, 0, 1, 0, 0);
 
-INSERT INTO foodIndex ('Food', 'Serving Size', 'Calories', 'Fat(grams)', 'Carbohydrates(grams)', 'Sugar(grams)', 'Protein', 'Sodium(milligrams', 'Fiber(grams)')
+INSERT INTO foodIndex (Food, Serving_Size, Calories, Fat_grams, Carbohydrates_grams, Sugar_grams, Protein, Sodium_milligrams, Fiber_grams)
 VALUES ('Splenda', '1 packet 1g)', 0, 0, 0.5, 0, 0, 0, 0);
 
-INSERT INTO foodIndex ('Food', 'Serving Size', 'Calories', 'Fat(grams)', 'Carbohydrates(grams)', 'Sugar(grams)', 'Protein', 'Sodium(milligrams', 'Fiber(grams)')
-VALUES ('Non-Dairy Creamer', 'Tsp (13.5g)', 10, 0, 1, 0, 0, 0, 0, 0);
+INSERT INTO foodIndex (Food, Serving_Size, Calories, Fat_grams, Carbohydrates_grams, Sugar_grams, Protein, Sodium_milligrams, Fiber_grams)
+VALUES ('Non-Dairy Creamer', 'Tsp (13.5g)', 10, 0, 1, 0, 0, 0, 0);
 
-INSERT INTO foodIndex ('Food', 'Serving Size', 'Calories', 'Fat(grams)', 'Carbohydrates(grams)', 'Sugar(grams)', 'Protein', 'Sodium(milligrams', 'Fiber(grams)')
+INSERT INTO foodIndex (Food, Serving_Size, Calories, Fat_grams, Carbohydrates_grams, Sugar_grams, Protein, Sodium_milligrams, Fiber_grams)
 VALUES ('Olive Oil', '1 Tbsp (13.5g)', 119, 14, 0, 0, 0, 0, 0);
 
-INSERT INTO foodIndex ('Food', 'Serving Size', 'Calories', 'Fat(grams)', 'Carbohydrates(grams)', 'Sugar(grams)', 'Protein', 'Sodium(milligrams', 'Fiber(grams)')
+INSERT INTO foodIndex (Food, Serving_Size, Calories, Fat_grams, Carbohydrates_grams, Sugar_grams, Protein, Sodium_milligrams, Fiber_grams)
 VALUES ('Cuties', '2 small', 80, 1, 17, 13, 1, 0, 4);
